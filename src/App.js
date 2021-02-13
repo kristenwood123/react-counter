e7b30ee324fc116e62e753cca6cbe53fa5cdfeef
Kristen <kristenwood123@gmail.com>
2021-02-12 17:44:03 -0800

    added styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Counter from './Counter'
+import './App.css'
 
 function App () {
-  const [count, setCount] = useState(1)  
-      // this.increment = this.increment.bind(this)
-    
-    const increment = () => {
-      setCount(count + 1)
-    }
+ const [count, setCount] = useState(0)
+
+ const increment = () => {
+   setCount(count + 1)
+ }
+
+const decrement = () => {
+   setCount(count - 1)
+ }
 
   return (
     <div className="App">
-      <Counter 
-        display={count} 
-        increment={() => {increment()}}/>      
+      <Counter
+      display={count}
+      decrement={decrement}
+      increment={increment} />
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+import './App.css'
 
- function Counter ({display, increment}) {
+ function Counter ({display, decrement, increment}) {
   
     return (
       <div className='counter'>
-        <span>{display}</span>
+      <button onClick={decrement}>-</button>
+        <h1>{display}</h1>
         <button onClick={increment}>+</button>
-      </div>
+       </div>
     )
     }
 
