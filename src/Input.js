b9c7e5144e2db15daa67066e83af0615f479bafa
Kristen <kristenwood123@gmail.com>
2021-02-13 17:48:46 -0800

    added clear function

--- src/Input.js
+++ src/Input.js
@@ -2,56 +2,28 @@ import { render } from '@testing-library/react'
 import React, { useState } from 'react'
 
 function Input () {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('Visitor')
+   
 
   const handleInput = e => {
     setInput(e.target.value)
   }
 
+  const handleClear = () => {
+    setInput('Visitor')
+  }
+
   return (
     <div>
-      <div>{input}</div>
+      <div>Hello, {input}</div>
       <input type="text"
         onChange={handleInput}
-
       />
-      <button>Clear</button>
+      <button
+      onClick={handleClear}>Clear</button>
     </div>
   )
 }
 
 export default Input
 
-
-
-
-
-
-
-
-//  constructor(props) {
-//    super(props)
-//    this.state = { value: ''};
-
-// this.handleInput = this.handleInput.bind(this);
-//  }
-
-//     handleInput = e => {
-//       this.setState({
-//         value: e.target.value
-//       })
-//  }
-
-//   render() {
-//   return (
-//     <div>
-//     <div>{this.state.value}</div>
-//       <input 
-//       type="text"
-//       onChange={this.handleInput}
-//       />
-//       <button>Clear</button>
-//     </div>
-    
-//     )
-//   }
